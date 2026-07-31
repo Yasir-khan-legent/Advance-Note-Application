363d47ea50f0a3844f4a67d8399e7533fcaa385c
Yasir-khan-legent <techpro54321@gmail.com>
2026-07-31 13:39:20 -0700

    change cors code

--- Backend/server.js
+++ Backend/server.js
@@ -10,11 +10,20 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
-const allowedOrigins = [process.env.FRONTEND_URL];
-app.use(cors({
-  origin: "*",
-}));
+const allowedOrigins = [
+  "https://advance-note-application-e4aq.vercel.app"
+];
 
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 
 Conection()
 
